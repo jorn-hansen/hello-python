6baaa349177e8c39f8678d34c52914b4007cc7fa
jorn-hansen <joernh@gmail.com>
2022-12-25 20:29:33 +0100

    add description

--- hellopython/__main__.py
+++ hellopython/__main__.py
@@ -1,2 +1,8 @@
+from sudoku import Sudokuboard
+import sys
+
 if __name__ == "__main__":
   print("Hello World in Python")
+  print("Add a Sudoku board, line by line:")
+  sudokuboard = Sudokuboard()
+  sudokuboard.input_board()
